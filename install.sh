d84b27f72ad1afa00593462d7b3b8c7dc7aa29a5
alexandre-Maury <alexandre_maury@outlook.com>
2024-10-26 20:24:34 +0200

    maj

--- install.sh
+++ install.sh
@@ -551,6 +551,38 @@ log_prompt "SUCCESS" && echo "Installation terminée." && echo ""
 arch-chroot ${MOUNT_POINT} mkinitcpio -p linux
 
 ##############################################################################
+## Configuration de PAM                                  
+##############################################################################
+log_prompt "INFO" && echo "Configuration de passwdqc.conf" && echo ""
+
+# Sauvegarde de l'ancien fichier passwdqc.conf
+if [ -f "${MOUNT_POINT}$PASSWDQC_CONF" ]; then
+    cp "${MOUNT_POINT}$PASSWDQC_CONF" "${MOUNT_POINT}$PASSWDQC_CONF.bak"
+    log_prompt "INFO" && echo "Sauvegarde du fichier existant passwdqc.conf en $PASSWDQC_CONF.bak" && echo ""
+fi
+
+# Génération du nouveau contenu de passwdqc.conf
+cat <<EOF > "${MOUNT_POINT}$PASSWDQC_CONF"
+min=$MIN
+max=$MAX
+passphrase=$PASSPHRASE
+match=$MATCH
+similar=$SIMILAR
+enforce=$ENFORCE
+retry=$RETRY
+EOF
+
+# Vérification du succès
+if [ $? -eq 0 ]; then
+    log_prompt "INFO" && echo "Fichier passwdqc.conf mis à jour avec succès." && echo ""
+    cat "$PASSWDQC_CONF"
+    log_prompt "SUCCESS" && echo "Terminée" && echo ""
+
+else
+    log_prompt "ERROR" && echo "Erreur lors de la mise à jour du fichier passwdqc.conf." && echo ""
+fi
+
+##############################################################################
 ## arch-chroot Création d'un mot de passe root                                             
 ##############################################################################
 # Demande tant que la réponse n'est pas y/Y ou n/N
