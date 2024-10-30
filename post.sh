27a764b2369b3e3d570e6c45173f337f9b578b95
alexandre-Maury <alexandre_maury@outlook.com>
2024-10-30 06:26:37 +0100

    maj

--- post.sh
+++ post.sh
@@ -2,12 +2,16 @@
 
 # script install.sh
 
+# https://github.com/Senshi111/debian-hyprland-hyprdots
+# https://github.com/nawfalmrouyan/hyprland
+
 set -e  # Quitte immédiatement en cas d'erreur.
 
 source config.sh # Inclure le fichier de configuration.
 source functions.sh  # Charge les fonctions définies dans le fichier fonction.sh.
 
 chmod +x *.sh # Rendre les scripts exécutables.
+# SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 ##############################################################################
 ## Information Utilisateur                                              
