25d2e1be9b498e201535981f32a4795018369b4e
alexandre-Maury <alexandre_maury@outlook.com>
2024-11-21 20:55:30 +0100

    maj

--- misc/arch/install.sh
+++ misc/arch/install.sh
@@ -17,33 +17,40 @@ PARTITION_TYPES=(
 )
 
 
-##############################################################################
-## Récupération des disques disponible                                                      
-##############################################################################
-LIST="$(lsblk -d -n | grep -v -e "loop" -e "sr" | awk '{print $1, $4}' | nl -s") ")" 
+select_disk() {
+    ##############################################################################
+    ## Récupération des disques disponible                                                      
+    ##############################################################################
+    LIST="$(lsblk -d -n | grep -v -e "loop" -e "sr" | awk '{print $1, $4}' | nl -s") ")" 
 
-if [[ -z "${LIST}" ]]; then
-    log_prompt "ERROR" && echo "Aucun disque disponible pour l'installation."
-    exit 1  # Arrête le script ou effectue une autre action en cas d'erreur
-else
-    log_prompt "INFO" && echo "Choisissez un disque pour l'installation (ex : 1) : " && echo ""
-    echo "${LIST}" && echo ""
-fi
-
-# Boucle pour que l'utilisateur puisse choisir un disque ou en entrer un manuellement
-OPTION=""
-while [[ -z "$(echo "${LIST}" | grep "  ${OPTION})")" ]]; do
-    log_prompt "INFO" && read -p "Votre Choix : " OPTION && echo ""
-    
-
-    # Vérification si l'utilisateur a entré un numéro (choix dans la liste)
-    if [[ -n "$(echo "${LIST}" | grep "  ${OPTION})")" ]]; then
-        # Si l'utilisateur a choisi un numéro valide, récupérer le nom du disque correspondant
-        DISK="$(echo "${LIST}" | grep "  ${OPTION})" | awk '{print $2}')"
-        break
+    if [[ -z "${LIST}" ]]; then
+        log_prompt "ERROR" && echo "Aucun disque disponible pour l'installation."
+        exit 1  # Arrête le script ou effectue une autre action en cas d'erreur
     else
-        # Si l'utilisateur a entré quelque chose qui n'est pas dans la liste, considérer que c'est un nom de disque
-        DISK="${OPTION}"
-        break
+        log_prompt "INFO" && echo "Choisissez un disque pour l'installation (ex : 1) : " && echo ""
+        echo "${LIST}" && echo ""
     fi
-done
+
+    # Boucle pour que l'utilisateur puisse choisir un disque ou en entrer un manuellement
+    OPTION=""
+    while [[ -z "$(echo "${LIST}" | grep "  ${OPTION})")" ]]; do
+        log_prompt "INFO" && read -p "Votre Choix : " OPTION && echo ""
+        
+
+        # Vérification si l'utilisateur a entré un numéro (choix dans la liste)
+        if [[ -n "$(echo "${LIST}" | grep "  ${OPTION})")" ]]; then
+            # Si l'utilisateur a choisi un numéro valide, récupérer le nom du disque correspondant
+            DISK="$(echo "${LIST}" | grep "  ${OPTION})" | awk '{print $2}')"
+            break
+        else
+            # Si l'utilisateur a entré quelque chose qui n'est pas dans la liste, considérer que c'est un nom de disque
+            DISK="${OPTION}"
+            break
+        fi
+    done
+}
+
+
+disk=$(select_disk)
+
+echo "Vous avez choisi le disque : $disk"
